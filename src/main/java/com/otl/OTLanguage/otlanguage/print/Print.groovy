b01061c150de8d447c2e24bf25291bceca43ff12
whiteha01 <whitefocus03@gmail.com>
2022-05-27 07:20:31 +0900

    commit

--- src/main/java/com/otl/OTLanguage/otlanguage/print/Print.groovy
+++ src/main/java/com/otl/OTLanguage/otlanguage/print/Print.groovy
@@ -6,12 +6,20 @@ import com.otl.OTLanguage.otlanguage.setting.Setting
 import java.util.regex.Pattern
 
 class Print extends Setting implements Check {
-    private static final String SPECIFIED = "ㅅㅁㅅ";
-    private final Pattern pattern = Pattern.compile("\\n\\s*ㅅㅁㅅ\\s|^\\s*ㅅㅁㅅ\\s");
+    private final String SPECIFIED = "ㅅㅁㅅ"
+    private final String PATTERN = "\\n\\s*ㅅㅁㅅ\\s|^\\s*ㅅㅁㅅ\\s"
+    private final Pattern pattern = Pattern.compile(PATTERN)
 
     @Override
     void start(String line) {
-
+        boolean bool = pattern.matcher(line).find()
+        if (bool) {
+            int start = line.indexOf(SPECIFIED)+SPECIFIED.length()
+            if (line.startsWith(SPECIFIED + " ")) start += 1
+            line = line.substring(start).strip()
+            total.append(line)
+            return
+        } else return
     }
 
     boolean check(String line) {
